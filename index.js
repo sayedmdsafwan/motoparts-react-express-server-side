3b74ecf4dbc4cae21d63239e401c946e1cd99605
Md. Safwan <sayedmdsafwan@gmail.com>
2022-05-24 20:51:40 +0600

    jwt authentication added

---
+++ index.js
@@ -0,0 +1,122 @@
+const express = require("express");
+const app = express();
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+require("dotenv").config();
+const port = process.env.PORT || 4000;
+const jwt = require("jsonwebtoken");
+const cors = require("cors");
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+// verify jwt
+function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send({ message: "UnAuthorized access" });
+    }
+    const token = authHeader.split(" ")[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+        if (err) {
+            return res.status(403).send({ message: "Forbidden access" });
+        }
+        req.decoded = decoded;
+        next();
+    });
+}
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.jhygh.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverApi: ServerApiVersion.v1,
+});
+async function run() {
+    try {
+        await client.connect();
+        const toolCollection = client.db("motoparts_bd").collection("tools");
+        const bookingCollection = client
+            .db("motoparts_bd")
+            .collection("bookings");
+        const userCollection = client.db("motoparts_bd").collection("users");
+
+        // get all items
+        // http://localhost:4000/tools
+        app.get("/tools", async (req, res) => {
+            const cursor = toolCollection.find({});
+            const result = await cursor.toArray();
+            res.send(result);
+        });
+
+        // get single item
+        // http://localhost:4000/tool/628c70f85367abcfc6535402
+        app.get("/tool/:id", async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await toolCollection.findOne(query);
+            res.send(result);
+        });
+
+        // get bookings by query
+        app.get("/booking", verifyJWT, async (req, res) => {
+            const buyer = req.query.buyer;
+            const decodedEmail = req.decoded.email;
+            if (buyer === decodedEmail) {
+                const query = { buyer: buyer };
+                const bookings = await bookingCollection.find(query).toArray();
+                return res.send(bookings);
+            } else {
+                return res.status(403).send({ message: "forbidden access" });
+            }
+        });
+
+        // post || get user info and booking details
+        app.post("/booking", async (req, res) => {
+            const booking = req.body;
+            const query = {
+                toolName: booking.toolName,
+                buyer: booking.buyer,
+                quantity: booking.quantity,
+            };
+            const exists = await bookingCollection.findOne(query);
+            if (exists) {
+                return res.send({ success: false, booking: exists });
+            }
+            const result = await bookingCollection.insertOne(booking);
+            return res.send({ success: true, result });
+        });
+
+        // post user by email || put method
+        app.put("/user/:email", async (req, res) => {
+            const email = req.params.email;
+            const user = req.body;
+            const filter = { email: email };
+            const options = { upsert: true };
+            const updateDoc = {
+                $set: user,
+            };
+            const result = await userCollection.updateOne(
+                filter,
+                updateDoc,
+                options
+            );
+            const token = jwt.sign(
+                { email: email },
+                process.env.ACCESS_TOKEN_SECRET,
+                { expiresIn: "1h" }
+            );
+            res.send({ result, token: token });
+        });
+    } finally {
+    }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+});
